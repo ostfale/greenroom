66c064a34ffe5846f9dc61c4f1f6318116b45562
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2026-08-29 18:51:18 +0200

    Initial commit

---
+++ src/test/java/de/ostfale/greenroom/GreenroomApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.greenroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
